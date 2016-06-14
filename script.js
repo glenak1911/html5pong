4e1f78a6c9e7eb00587b6549c0b58367afd15331
Glen Knight <glenak1911@gmail.com>
2016-06-14 08:15:25 -0400

    updated script

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ var ball;
 window.onload = function game(){
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
-	drawElements();
+	setInterval(drawElements,10);
 }
 
 function drawElements(){
+	ball+=5;
 	canvasContext.fillStyle = 'black';
 	canvasContext.fillRect(0,0,canvas.width,canvas.height);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(255,210,200,200);
+	canvasContext.fillStyle = 'red';
+	canvasContext.fillRect(ballx,100,10,10);
 }
