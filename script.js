0e91c324667a3d45df7235cb32a7d11027d47072
Glen Knight <glenak1911@gmail.com>
2016-06-16 00:34:14 -0400

    Separated moving and drawing functions, and added to play() function

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ window.onload = function game(){
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 	ballx=100;
-	setInterval(drawElements,10);
+	setInterval(play,10);
+}
+
+function play(){
+	drawElements();
+	movingParts();
 }
 
 function drawElements(){
-	ballx+=5;
 	canvasContext.fillStyle = 'black';
 	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 	canvasContext.fillStyle = 'red';
@@ -19,4 +23,8 @@ function drawElements(){
 	canvasContext.fillRect(0, 210, 10, 100);
 	canvasContext.fillStyle = 'white';
 	canvasContext.fillRect(canvas.width-10, 210, 10, 100);
+}
+
+function movingParts(){
+	ballx+=5;
 }
