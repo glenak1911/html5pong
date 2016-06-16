b4dbaf3fdd48112a6af0e6bcdbfa6196e8c3d18f
Glen Knight <glenak1911@gmail.com>
2016-06-16 00:30:35 -0400

    Created paddle prototypes

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ var ballx;
 window.onload = function game(){
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
+	ballx=100;
 	setInterval(drawElements,10);
 }
 
 function drawElements(){
+	ballx+=5;
 	canvasContext.fillStyle = 'black';
 	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 	canvasContext.fillStyle = 'red';
-	canvasContext.fillRect(100, 100, 25, 20);
+	canvasContext.fillRect(ballx, 100, 25, 20);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(0, 210, 10, 100);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(canvas.width-10, 210, 10, 100);
 }
